a467b4809644326fcf30b38660940870ee5e24e1
TheKingsident <kingsident@gmail.com>
2024-01-01 20:37:35 +0100

    Updated 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,33 +1,12 @@
-# Retrieve the home directory path of the user running Puppet
-$user_home = $::home
-
 # Configures the ssh config file
-file_line { 'ssh_config_myserverFromAlx':
-  path  => "${user_home}/.ssh/config",
-  line  => 'Host myserverFromAlx',
-  match => '^Host myserverFromAlx$',
-}
-file_line { 'ssh_config_myserverFromAlx_hostname':
-  path    => "${user_home}/.ssh/config",
-  line    => '    HostName 54.236.27.95',
-  match   => '^    HostName \S+',
-  require => File_line['ssh_config_myserverFromAlx'],
-}
-file_line { 'ssh_config_myserverFromAlx_user':
-  path    => "${user_home}/.ssh/config",
-  line    => '    User ubuntu',
-  match   => '^    User \S+',
-  require => File_line['ssh_config_myserverFromAlx_hostname'],
-}
-file_line { 'ssh_config_myserverFromAlx_identityfile':
-  path    => "${user_home}/.ssh/config",
-  line    => '    IdentityFile ~/.ssh/school',
-  match   => '^    IdentityFile \S+',
-  require => File_line['ssh_config_myserverFromAlx_user'],
-}
-file_line { 'ssh_config_myserverFromAlx_passwordauth':
-  path    => "${user_home}/.ssh/config",
-  line    => '    PasswordAuthentication no',
-  match   => '^    PasswordAuthentication',
-  require => File_line['ssh_config_myserverFromAlx_identityfile'],
+file { '/etc/ssh/ssh_config':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "
+Host *
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no
+  ",
 }
