abea553909b38458113f8a21d79b35f6f42978d4
TheKingsident <kingsident@gmail.com>
2024-02-15 01:33:52 +0100

    Added new files

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script defines a function that returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a subreddit.
+    """
+    api_link = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'SubredditSubscriberCounter/0.1 by Kingsident'}
+
+    response = requests.get(api_link, headers=headers, allow_redirects=False)
+
+    if not response.ok:
+        return 0
+
+    data = response.json()
+    subscribers = data.get('data', {}).get('subscribers', 0)
+
+    return subscribers if subscribers else 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+This directory contains assignment files for the " 0x16. API advanced" task from ALX
