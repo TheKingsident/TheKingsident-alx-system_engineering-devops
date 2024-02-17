d3d7432495ab9c83fe81000b52303d645f1fe824
TheKingsident <kingsident@gmail.com>
2024-02-17 19:55:51 +0100

    Updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,12 @@
 
 $target_file = '/var/www/html/wp-settings.php'
 
+exec{ 'correction':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+
 file_line { 'correct_class_wp_locale_reference':
   ensure => present,
   path   => $target_file,
