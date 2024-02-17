a89e6f5c02432df754af53b7c6f1f076c94cd998
TheKingsident <kingsident@gmail.com>
2024-02-17 19:57:21 +0100

    Update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -6,11 +6,3 @@ exec{ 'correction':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
-
-
-file_line { 'correct_class_wp_locale_reference':
-  ensure => present,
-  path   => $target_file,
-  match  => '^.*class-wp-locale.phpp.*$',
-  line   => 'require_once(ABSPATH . WPINC . "/class-wp-locale.php");',
-}
