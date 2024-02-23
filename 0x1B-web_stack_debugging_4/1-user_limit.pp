ea4073bff2840446aea0fb9ebeabe201748c7335
TheKingsident <kingsident@gmail.com>
2024-02-23 20:03:32 +0100

    Updated

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Lets holberton user log in and open files
 
-exec { 'incereases hard-limit':
+exec { 'increases-hard-limit':
   command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin/:/bin/'
 }
 
-exec { 'incereases soft-limit':
+exec { 'increases-soft-limit':
   command => "sed -i '/^holberton soft/s/5/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin/:/bin/'
 }
