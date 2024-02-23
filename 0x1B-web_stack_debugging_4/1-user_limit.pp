e771b8127329a8e82e0e743e0642a7c1cda2d431
TheKingsident <kingsident@gmail.com>
2024-02-23 20:09:39 +0100

    Updated

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,10 +2,12 @@
 
 exec { 'increases-hard-limit':
   command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/'
+  path    => ['/usr/local/bin/', '/bin/'],
+  onlyif  => "grep '^holberton hard' /etc/security/limits.conf | grep -q '4'",
 }
 
 exec { 'increases-soft-limit':
   command => "sed -i '/^holberton soft/s/5/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/'
+  path    => ['/usr/local/bin/', '/bin/'],
+  onlyif  => "grep '^holberton soft' /etc/security/limits.conf | grep -q '5'",
 }
