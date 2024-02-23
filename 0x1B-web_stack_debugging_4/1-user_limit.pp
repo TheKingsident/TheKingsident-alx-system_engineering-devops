a7b3482f25d2ae4b34a5836f6dfcdb5843a01eb4
TheKingsident <kingsident@gmail.com>
2024-02-23 20:22:34 +0100

    Updated

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,20 +4,16 @@ user { 'holberton':
 }
 
 group { 'sudo':
+  ensure  => present,
   members => ['holberton'],
 }
 
-# Update sudoers file to allow members of the sudo group to execute commands without a password
+# Allow members of the sudo group to execute commands without a password
 file { '/etc/sudoers':
-  content => template('yourmodule/sudoers.erb'),
-  mode    => '0440',
-  owner   => 'root',
-  group   => 'root',
-  notify  => Exec['validate_sudoers'],
-}
-
-exec { 'validate_sudoers':
-  command     => 'visudo -c',
-  refreshonly => true,
-  subscribe   => File['/etc/sudoers'],
+  ensure       => file,
+  content      => "# This file is managed by Puppet. DO NOT EDIT.\n\n%sudo ALL=(ALL:ALL) NOPASSWD: ALL\n",
+  mode         => '0440',
+  owner        => 'root',
+  group        => 'root',
+  validate_cmd => 'visudo -cf %',
 }
